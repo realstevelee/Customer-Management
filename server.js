f34e4d7211605d8fd1f514ac44142c1ca6713c81
realstevelee <realstevelee@gmail.com>
2020-07-01 23:10:16 -0400

    add upload feature working with server

--- server.js
+++ server.js
@@ -21,6 +21,9 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+const multer = require('multer');
+const upload = multer({dest: './upload'})
+
 app.get("/api/customers", (req, res) => {
   connection.query(
     "SELECT * FROM CUSTOMER",
@@ -30,4 +33,25 @@ app.get("/api/customers", (req, res) => {
   )
 })
 
+app.use('/image', express.static('./upload'));
+
+app.post('/api/customers', upload.single('image'), (req,res) => {
+  let sql = "INSERT INTO CUSTOMER VALUES (NULL, ?, ?, ?, ?, ?)";
+  let image =  'http://localhost:5000/image/' + req.file.filename;
+
+  let name = req.body.name;
+  let birthday = req.body.birthday;
+  let gender = req.body.gender;
+  let job = req.body.job;
+
+  let params = [image, name, birthday, gender, job];
+
+  connection.query(sql, params,
+    (err, rows, fileds) => {
+      res.send(rows);
+      console.log(err)
+    })
+})
+
 app.listen(port, () => console.log(`Lisenting on port ${port}`))
+
